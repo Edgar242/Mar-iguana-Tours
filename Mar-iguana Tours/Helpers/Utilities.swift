29e895343a332ae3662edb35ca67aa28b5701036
Edgar242 <edgar.zamora.aranda@gmail.com>
2021-03-09 08:39:05 -0600

    Added function to validate textField

--- Mar-iguana Tours/Helpers/Utilities.swift
+++ Mar-iguana Tours/Helpers/Utilities.swift
@@ -5,4 +5,17 @@
 //  Created by Edgar on 09/03/21.
 //
 
+import UIKit
 import Foundation
+
+class Utilities {
+    static var errorTextFieldEmpty = "Por favor llene todos los campos"
+    
+    static func validateTextField(_ textField: UITextField) -> String? {
+        guard textField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" else {
+            return nil
+        }
+        
+        return errorTextFieldEmpty
+    }
+}
