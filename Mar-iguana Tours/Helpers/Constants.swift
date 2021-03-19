7c10275bc4e3af30fc39d2b48f91c229e5d353c5
Edgar242 <edgar.zamora.aranda@gmail.com>
2021-03-19 11:20:21 -0600

    Variable renaming

--- Mar-iguana Tours/Helpers/Constants.swift
+++ Mar-iguana Tours/Helpers/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Constants {
     struct Storyboard {
         static let viewControllerProfile = "ViewControllerProfile"
-        static let navControllerProfile = "NavigationControllerProfile"
+        static let viewControllerLoginOrRegister = "LoginOrRegister"
     }
     
     static let cadenaVacia:String = ""
